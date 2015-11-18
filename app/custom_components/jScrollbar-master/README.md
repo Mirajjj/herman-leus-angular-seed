8e2621f742b5ac935a1fef6be046d4bbc950b702
German Leus <German.Leus@dataart.com>
2015-11-18 17:10:46 +0200

    update

---
+++ app/custom_components/jScrollbar-master/README.md
@@ -0,0 +1,5 @@
+# Readme
+
+Full documentation on [MyJqueryPlugins](http://www.myjqueryplugins.com/jScrollbar)
+
+Demonstration on [jScrollbar demonstration page](http://www.myjqueryplugins.com/jScrollbar/demo)
--- app/directives/directives.js
+++ app/directives/directives.js
@@ -6,11 +6,44 @@
     var directives = angular.module('mainModule.directives', []),
         isMouseOver = false;
 
+    directives.directive('jqueryCustomFuncs', function () {
+        return function (scope, elm, attrs) {
+            $.fn.strech_text = function () {
+                var elmt          = $(this),
+                    cont_width    = elmt.width(),
+                    txt           = elmt.text(),
+                    one_line      = $('<span class="stretch_it">' + txt + '</span>'),
+                    nb_char       = elmt.text().length,
+                    spacing       = cont_width / nb_char,
+                    txt_width;
+
+                elmt.html(one_line);
+                txt_width = one_line.width();
+
+                if (txt_width < cont_width) {
+                    var char_width     = txt_width / nb_char,
+                        ltr_spacing    = spacing - char_width + (spacing - char_width) / nb_char;
+
+                    one_line.css({'letter-spacing': ltr_spacing});
+                } else {
+                    one_line.contents().unwrap();
+                    elmt.addClass('justify');
+                }
+            };
+        };
+    });
+
     directives.directive('canvasHomepage', ['homepageCanvas', function (Initialization) {
         return function (scope, elm, attrs) {
-            var init;
+            var init,
+                resize = function () {
+                    $(elm).width($(window).width());
+                    $(elm).height($(window).height());
+                };
 
-            $(elm[0]).width($(window).width());
+            resize();
+
+            $(window).resize(resize);
 
             init = new Initialization(elm[0]);
         };
@@ -48,13 +81,13 @@
             $(elm).hover(
                 function () {
                     img.stop().animate({
-                        'margin-right': visible,
+                        'margin-left': visible,
                         'opacity': 1
                     }, 500);
                 },
                 function () {
                     img.stop().animate({
-                        'margin-right': hidden,
+                        'margin-left': hidden,
                         'opacity': 0
                     }, 300);
 
@@ -78,10 +111,35 @@
 
     directives.directive('introductionContainer', [function () {
         return function (scope, elm, attrs) {
-            $(elm).animate({
-                'top': ($(window).height() * 0.55) + 'px',
-                'opacity': 1
-            }, 500);
+            var resize = function () {
+                    $(elm).find('h1').strech_text();
+
+                    $(elm).stop().animate({
+                        'top': ($(window).height() - 120) + 'px',
+                        'opacity': 1
+                    });
+                };
+
+            resize();
+            $(window).resize(resize);
         };
     }]);
+
+    directives.directive('webTitles', [function () {
+        return function (scope, elm, attrs) {
+            var resize = function () {
+
+                $(elm).stop().animate({
+                    'left': '10%',
+                    'opacity': 1
+                }, {
+                    duration: 500
+                });
+            };
+
+            resize();
+            $(window).resize(resize);
+        };
+    }]);
+
 })();
